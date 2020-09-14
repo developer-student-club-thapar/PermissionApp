32dfb5122eb7ee3dc4cac82bd6bbc634819e8959
akgupta-47 <60443592+akgupta-47@users.noreply.github.com>
2020-09-14 22:05:29 +0530

    Update http-error.js

--- backend/models/http-error.js
+++ backend/models/http-error.js
@@ -2,6 +2,10 @@ class HttpError extends Error {
   constructor(message, errorCode) {
     super(message); // Add a "message" property
     this.code = errorCode; // Adds a "code" property
+    this.status = `${errorCode}`.startsWith('4') ? 'fail' : 'error';
+    this.isOperational = true;
+
+    Error.captureStackTrace(this, this.constructor);
   }
 }
 
